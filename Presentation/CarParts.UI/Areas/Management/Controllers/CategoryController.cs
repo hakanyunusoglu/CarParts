df09d91844256ecdf9f552841d993b6c63b30577
hakanyunusoglu93@gmail.com <hakanyunusoglu93@gmail.com>
2022-03-28 14:53:02 +0300

    Kategori sayfaları eklenmeye başlandı

---
+++ Presentation/CarParts.UI/Areas/Management/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarParts.UI.Areas.Management.Controllers
+{
+    [Area("Management")]
+    public class CategoryController : Controller
+    {
+        public IActionResult ListCategory()
+        {
+            return View();
+        }
+        public IActionResult NewCategory()
+        {
+            return View();
+        }
+        public IActionResult UpdateCategory()
+        {
+            return View();
+        }
+        public IActionResult DeleteCategory()
+        {
+            return View();
+        }
+    }
+}
